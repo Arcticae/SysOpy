32a3b13cbf3b9d36b3b2cc59fa0275490ad40823
Arcticae <tomekgsd@gmail.com>
2018-03-13 20:51:00 +0100

    created libs

---
+++ lab1/library.c
@@ -0,0 +1,7 @@
+//
+// Created by timelock on 13.03.18.
+//
+
+#include "library.h"
+
+
---
+++ lab1/library.h
@@ -0,0 +1,13 @@
+//
+// Created by timelock on 13.03.18.
+//
+
+#ifndef PROJECT_LIBRARY_H
+#define PROJECT_LIBRARY_H
+
+
+
+
+
+
+#endif //PROJECT_LIBRARY_H
