86e66905d976cddfa5b49f8b1b20514be6e4b96d
Arcticae <tomekgsd@gmail.com>
2018-03-22 22:16:34 +0100

    chyba juz szysko

--- lab2/zad1/main.c
+++ lab2/zad1/main.c
@@ -2,14 +2,22 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <limits.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #define LIB_MODE 11
 #define SYS_MODE 22
 
 
 int generate(char*dest_path,int record_amount,int record_length){
     FILE * file = fopen(dest_path,"w+");    // overwrites the existing file if exists
-    FILE * random= fopen("/dev/random","r");
-
+    FILE * random= fopen("/dev/urandom","r");
+    if (file==NULL || random==NULL){
+        printf("Error while generating the file, error number : %d (%s)\n", errno, strerror(errno));
+        return -1;
+    }
     char*buffer=malloc((record_length+1)*sizeof(char));
 
     int i;
@@ -117,7 +125,7 @@ int system_sort(char*source_path,int record_amount,int record_length){
 
     if(source<0){
         printf("An error occurred while opening the file, please check your choice one more time\n");
-        return 1;
+        return -1;
     }
 
 
@@ -177,9 +185,9 @@ int lib_copy(char*source,char*dest,int record_amount,int record_length){
     FILE*from=fopen(source,"r");
     FILE*to=fopen(dest,"w+");
 
-    if(from==NULL| to == NULL){
+    if(from==NULL || to == NULL){
         printf("No such file is available for copying, please check your choice one more time\n");
-        return 1;
+        return -1;
     }
 
     char*record=malloc((record_length+1)*sizeof(char));
@@ -212,9 +220,9 @@ int sys_copy(char*source,char*dest,int record_amount,int record_length){
     int from=open(source,O_RDONLY);
     int to=open(dest,O_WRONLY|O_TRUNC);
 
-    if(from<0 | to <0){
+    if(from<0 || to <0){
         printf("No such file is available for copying, please check your choice one more time\n");
-        return 1;
+        return -1;
     }
 
     char*record=malloc((record_length+1)*sizeof(char));
@@ -238,7 +246,14 @@ int sys_copy(char*source,char*dest,int record_amount,int record_length){
     close(from);
     free(record);
 }
-void copy_join(char*source,char*dest,int record_amount,int record_length,int mode){
+void copy_join(char*source_raw,char*dest_raw,int record_amount,int record_length,int mode){
+
+    char source[PATH_MAX];
+    realpath(source_raw,source);
+
+    char dest[PATH_MAX];
+    realpath(dest_raw,dest);
+
     if(mode==SYS_MODE){
         if(sys_copy(source,dest,record_amount,record_length)<0){
             printf("There was an error while system-mode copying. Operation failed\n");
@@ -253,7 +268,12 @@ void copy_join(char*source,char*dest,int record_amount,int record_length,int mod
         printf("Wrong mode-type chosen. choose \"LIB_MODE\" or \"SYS_MODE\" as an argument to operation\n");
     }
 }
-void sort_join(char*source,int record_amount,int record_length,int mode){
+void sort_join(char*source_raw,int record_amount,int record_length,int mode){
+
+    char source[PATH_MAX];
+    realpath(source_raw,source);
+
+
     if(mode==SYS_MODE){
         if(system_sort(source,record_amount,record_length)<0){
             printf("There was an error while system-mode copying. Operation failed\n");
@@ -269,7 +289,126 @@ void sort_join(char*source,int record_amount,int record_length,int mode){
     }
 }
 
-int main() {
+int main(int argc,char**argv){
+
+    struct timeval* user_times=malloc(2*sizeof(struct timeval));
+    struct timeval* sys_times=malloc(2*sizeof(struct timeval));
+    struct timeval** real_times=malloc(2*sizeof(struct timeval*));
+
+    for(int i=0;i<2;i++){
+
+        real_times[i]=(struct timeval*)malloc(sizeof(struct timeval*));
+    }
+
+
+    struct rusage* before=(struct rusage*)malloc(sizeof(struct rusage));
+    struct rusage* after=(struct rusage*)malloc(sizeof(struct rusage));
+
+
+
+
+   if(argc<5){
+        printf("There are no enough arguments. Give me at least 4\n");
+        return -1;
+    }
+
+    if(strcmp(argv[1],"generate")==0){
+        int record_amount=(int)strtol(argv[3],NULL,10);
+        int record_length=(int)strtol(argv[4],NULL,10);
+
+        gettimeofday(real_times[0],NULL);
+        getrusage(RUSAGE_SELF,before);
+
+        generate(argv[2],record_amount,record_length);
+
+        gettimeofday(real_times[1],NULL);
+        getrusage(RUSAGE_SELF,after);
+    }
+    else if(strcmp(argv[1],"sort")==0){
+
+        if(argc<6){
+            printf("There are no enough arguments. Give me at least 5 for sorting\n");
+            return -1;
+        }
+        int record_amount=(int)strtol(argv[3],NULL,10);
+        int record_length=(int)strtol(argv[4],NULL,10);
+            if(strcmp(argv[5],"LIB_MODE")==0)
+            {
+                gettimeofday(real_times[0],NULL);
+                getrusage(RUSAGE_SELF,before);
+
+                sort_join(argv[2],record_amount,record_length,LIB_MODE);
+
+                gettimeofday(real_times[1],NULL);
+                getrusage(RUSAGE_SELF,after);
+
+            }
+            else if(strcmp(argv[5],"SYS_MODE")==0)
+            {
+                gettimeofday(real_times[0],NULL);
+                getrusage(RUSAGE_SELF,before);
+
+                sort_join(argv[2],record_amount,record_length,SYS_MODE);
+
+                gettimeofday(real_times[1],NULL);
+                getrusage(RUSAGE_SELF,after);
+            }
+            else {
+                printf("Wrong mode-type chosen. choose \"LIB_MODE\" or \"SYS_MODE\" as an argument to operation\n");
+                return -1;
+            }
+        }else if(strcmp(argv[1],"copy")==0){
+
+        if(argc<7){
+            printf("There are no enough arguments. Give me at least 6 for copying and pasting\n");
+            return -1;
+        }
+
+        int record_amount=(int)strtol(argv[4],NULL,10);
+        int record_length=(int)strtol(argv[5],NULL,10);
+
+        if(strcmp(argv[6],"LIB_MODE")==0)
+        {
+            gettimeofday(real_times[0],NULL);
+            getrusage(RUSAGE_SELF,before);
+
+            copy_join(argv[2],argv[3],record_amount,record_length,LIB_MODE);
+
+            gettimeofday(real_times[1],NULL);
+            getrusage(RUSAGE_SELF,after);
+        }
+        else if(strcmp(argv[6],"SYS_MODE")==0)
+        {
+            gettimeofday(real_times[0],NULL);
+            getrusage(RUSAGE_SELF,before);
+
+            copy_join(argv[2],argv[3],record_amount,record_length,SYS_MODE);
+
+            gettimeofday(real_times[1],NULL);
+            getrusage(RUSAGE_SELF,after);
+        }
+        else {
+            printf("Wrong mode-type chosen. choose \"LIB_MODE\" or \"SYS_MODE\" as an argument to operation\n");
+            return -1;
+        }
+    }
+
+    sys_times[0]=before->ru_stime;
+    sys_times[1]=after->ru_stime;
+
+    user_times[0]=before->ru_utime;
+    user_times[1]=after->ru_utime;
+
+    if(strcmp(argv[1],"copy")==0){
+        printf("\nInvoking operation %s with arguments: \n%d of entries\nwhich are %d characters long",argv[1],(int)strtol(argv[4],NULL,10),(int)strtol(argv[5],NULL,10));
+    }else{
+        printf("\nInvoking operation %s with arguments: \n%d of entries\nwhich are %d characters long",argv[1],(int)strtol(argv[3],NULL,10),(int)strtol(argv[4],NULL,10));
+    }
+
+    printf("\nInvoking operation %s with arguments\n",argv[1]);
+    printf("Realtime: %ld.%06lds\n",real_times[1]->tv_sec-real_times[0]->tv_sec,real_times[1]->tv_usec-real_times[0]->tv_usec);
+    printf("System Time: %ld.%06lds\n",sys_times[1].tv_sec-sys_times[0].tv_sec,sys_times[1].tv_usec-sys_times[0].tv_usec);
+    printf("User Time: %ld.%06lds\n\n",user_times[1].tv_sec-user_times[0].tv_sec,user_times[1].tv_usec-user_times[0].tv_usec);
 
 
     return 0;
--- lab2/zad2/main.c
+++ lab2/zad2/main.c
@@ -1,8 +1,11 @@
 //
 // Created by timelock on 21.03.18.
 #define __USE_XOPEN
-#define _XOPEN_SOURCE
+#define _XOPEN_SOURCE 500
 #define _GNU_SOURCE
+#define NFTW_MODE 11
+#define MANUAL_MODE 22
+
 #include <limits.h>
 #include <time.h>
 #include <sys/stat.h>
@@ -12,6 +15,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ftw.h>
+#
 const char format[] = "%Y-%m-%d %H:%M:%S";
 
 char*tmp_operand;
@@ -55,23 +59,25 @@ time_t compare_days(time_t mod_time,time_t given_date){
     return -1; */
 }
 void show_file(const char*file_path,const struct stat*file_stats){
+    if(S_ISREG(file_stats->st_mode))
+    {
+        printf(" %lld\t", file_stats->st_size);//todo
+        printf((file_stats->st_mode & S_IRUSR) ? "r" : "-");
+        printf((file_stats->st_mode & S_IWUSR) ? "w" : "-");
+        printf((file_stats->st_mode & S_IXUSR) ? "x" : "-");
+        printf((file_stats->st_mode & S_IRGRP) ? "r" : "-");
+        printf((file_stats->st_mode & S_IWGRP) ? "w" : "-");
+        printf((file_stats->st_mode & S_IXGRP) ? "x" : "-");
+        printf((file_stats->st_mode & S_IROTH) ? "r" : "-");
+        printf((file_stats->st_mode & S_IWOTH) ? "w" : "-");
+        printf((file_stats->st_mode & S_IXOTH) ? "x" : "-");
 
-    printf(" %lld\t", file_stats->st_size);//todo
-    printf((file_stats->st_mode & S_IRUSR) ? "r" : "-");
-    printf((file_stats->st_mode & S_IWUSR) ? "w" : "-");
-    printf((file_stats->st_mode & S_IXUSR) ? "x" : "-");
-    printf((file_stats->st_mode & S_IRGRP) ? "r" : "-");
-    printf((file_stats->st_mode & S_IWGRP) ? "w" : "-");
-    printf((file_stats->st_mode & S_IXGRP) ? "x" : "-");
-    printf((file_stats->st_mode & S_IROTH) ? "r" : "-");
-    printf((file_stats->st_mode & S_IWOTH) ? "w" : "-");
-    printf((file_stats->st_mode & S_IXOTH) ? "x" : "-");
-
-    char temptime[20];
-    strftime(temptime,20 , format, localtime(&file_stats->st_mtime));
-    printf(" %s\t", temptime);
-    printf(" %s\t", realpath(file_path, NULL));
-    printf("\n");
+        char temptime[20];
+        strftime(temptime, 20, format, localtime(&file_stats->st_mtime));
+        printf(" %s\t", temptime);
+        printf(" %s\t", realpath(file_path, NULL));
+        printf("\n");
+    }
 
 }
 void manual_display(char*path,char*operand,time_t given_time){
@@ -103,7 +109,7 @@ void manual_display(char*path,char*operand,time_t given_time){
 
             if(strcmp(dir_entry->d_name,".")!=0 && strcmp(dir_entry->d_name,"..")!=0 )
             {
-                    if(S_ISREG(file_stats.st_mode)){
+
                         if (strcmp(operand, "=") == 0 && compare_days(file_stats.st_mtim.tv_sec, given_time) == 0) {
                             show_file(element_path, &file_stats);
                         }
@@ -113,7 +119,7 @@ void manual_display(char*path,char*operand,time_t given_time){
                         if (strcmp(operand, "<") == 0 && compare_days(file_stats.st_mtim.tv_sec, given_time) < 0) {
                             show_file(element_path, &file_stats);
                         }
-                    }
+
                 if(S_ISDIR(file_stats.st_mode)){
                     manual_display(element_path,operand,given_time);
                 }
@@ -134,31 +140,50 @@ void manual_display(char*path,char*operand,time_t given_time){
 
 int nftw_show(const char*path,const struct stat* file_stats,int typeflag,struct FTW*ftwb){
 
-    if(strcmp(tmp_operand,"=")==0 && compare_days(file_stats->st_mtim.tv_sec,tmp_time)==0){
-        show_file(path,file_stats);
-        return 0;
-    }
-    if(strcmp(tmp_operand,">")==0 && compare_days(file_stats->st_mtim.tv_sec,tmp_time)>0){
-        show_file(path,file_stats);
-        return 0;
-    }
-    if(strcmp(tmp_operand,"<")==0 && compare_days(file_stats->st_mtim.tv_sec,tmp_time)<0){
-        show_file(path,file_stats);
-        return 0;
-    }
+    if(typeflag==FTW_F) {
 
+        if (strcmp(tmp_operand, "=") == 0 && compare_days(file_stats->st_mtim.tv_sec, tmp_time) == 0) {
+            show_file(path, file_stats);
+        }
+        if (strcmp(tmp_operand, ">") == 0 && compare_days(file_stats->st_mtim.tv_sec, tmp_time) > 0) {
+            show_file(path, file_stats);
+        }
+        if (strcmp(tmp_operand, "<") == 0 && compare_days(file_stats->st_mtim.tv_sec, tmp_time) < 0) {
+            show_file(path, file_stats);
+        }
+    }
+    return 0;
 }
+void view_choice(const char*path,char*operand,char*given_time,int mode){
+
+    char*real_path=realpath(path,NULL);
+
+    struct tm*time_struct=malloc(sizeof(struct tm));
+    strptime(given_time,format,time_struct);
+    time_t tmp_epoch_time=mktime(time_struct);
 
 
+
+    if(mode==NFTW_MODE){
+        tmp_operand=operand;
+        tmp_time=tmp_epoch_time;    //assigning the globals
+
+        nftw(real_path,nftw_show,10,FTW_PHYS);
+
+    }else if(mode==MANUAL_MODE){
+        manual_display(real_path,operand,tmp_epoch_time);
+    }
+
+    free(real_path);
+    free(time_struct);
+}
 
 int main(int argc,char**argv){
 
-    tmp_time=time(NULL);
-    manual_display(realpath("..",NULL),"<",tmp_time);
-    printf("\n");
-    tmp_operand="<";
-    nftw(realpath("/home/timelock/Pobrane",NULL),nftw_show,10,FTW_PHYS);    //dziawa hihi
 
-    time(NULL);
+
+    //view_choice("../","<","2018-03-21 16:34:23",MANUAL_MODE);//example use
+    printf("\n\n\n");
+    view_choice("../","<","2018-03-21 16:34:23",NFTW_MODE);//example use
     return 0;
 }
