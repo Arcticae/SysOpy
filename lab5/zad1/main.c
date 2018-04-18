ed3bf47dee0cabe190ecc8bdda760b1c7576755b
Arcticae <tomekgsd@gmail.com>
2018-04-18 19:16:10 +0200

    dziejo sie rzeczy

--- lab5/zad1/main.c
+++ lab5/zad1/main.c
@@ -1,6 +1,4 @@
-//
-// Created by timelock on 28.03.18.
-//
+#define _BSD_SOURCE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,9 +6,9 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define maxline_len 100
-#define max_argnum 10
-#define max_arglen 10
+#define maxline_len 512
+#define max_argnum 100
+#define max_arglen 100
 
 char**get_args(char*line){
 
@@ -29,7 +27,7 @@ char**get_args(char*line){
 int handle_oneliner(char*line){
 
     char*arguments[max_argnum];
-    int executables=0,i=0,status;
+    int executables=0,i=0;
     pid_t pid;
 
     while((arguments[executables]=strtok(executables == 0 ? line : NULL,"|"))!=NULL)executables++;
@@ -44,8 +42,8 @@ int handle_oneliner(char*line){
             exit(EXIT_FAILURE);
         }
 
-
-        if((pid=fork())==0){
+        pid=fork();
+        if(pid==0){
 
             if(i!=executables-1){       //if it's not the last one you have to redirect the output
 
@@ -57,29 +55,30 @@ int handle_oneliner(char*line){
             if(i>0){
 
                 if(dup2(file_descriptors[i-1][0],STDIN_FILENO)==-1) {
-                        fprintf(stderr,"Error while doing the stuff with dup2 (IN)%d!",i);
-                        exit(EXIT_FAILURE);
-                    }
+                    fprintf(stderr,"Error while doing the stuff with dup2 (IN)%d!",i);
+                    exit(EXIT_FAILURE);
+                }
             }
 
 
 
-                char**line_args=get_args(arguments[i]);
-                execvp(line_args[0],line_args);
-                perror("Piped task has encountered an error!");
-                exit(EXIT_SUCCESS);
+            char**line_args=get_args(arguments[i]);
+            execvp(line_args[0],line_args);
+            perror("Piped task has encountered an error!");
+            exit(EXIT_SUCCESS);
 
-       }else{
+        }else{
             if(i<executables-1)close(file_descriptors[i][1]);  //closing the unused write end last needs to print to stdout the result
             if(i>0)close(file_descriptors[i-1][0]);    //previous in pipe needs to be closed
         }
 
     }
-        wait(NULL);
+    wait(NULL);
 
-        return 0;
+    return 0;
 
 }
+
 
 
 int main(int argc, char**argv){
@@ -95,15 +94,15 @@ int main(int argc, char**argv){
         return -1;
     }
     char line[maxline_len];
-    int status=0;
-    pid_t pid;
 
     while(fgets(line,maxline_len,batch_file)){
+        fprintf(stderr,"\tDEBUG: %s\n",line);
+        int status=0;
+        pid_t pid;
+        pid=fork();
 
-
-        if((pid=fork())==0){
+        if(pid==0){
             handle_oneliner(line);      //execution
-
             exit(EXIT_SUCCESS);
         }
 
@@ -113,8 +112,9 @@ int main(int argc, char**argv){
             perror(NULL);
             return -1;
         }
-        exit(EXIT_SUCCESS);
+
     }
+
 
     fclose(batch_file);
     return 0;
