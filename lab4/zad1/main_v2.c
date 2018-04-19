c427c767e68c612d0c57dd0c9a7797e4f1d4d138
Arcticae <tomekgsd@gmail.com>
2018-04-19 03:10:25 +0200

    Script filepath added because mr Kurdziel didnt notice that you should change it and didnt really give a single f

--- lab4/zad1/main_v2.c
+++ lab4/zad1/main_v2.c
@@ -39,13 +39,20 @@ void sigtstp_handler(int sigtstp){
         script_working=0;
     }
 }
+void child_died(int sig){
+    fprintf(stderr,"Execution of the child process was not possible, please check your args\n");
+    exit(EXIT_FAILURE);
+}
 int main(int argc, char** argv) {//sigint = signal, sigtstp= sigaction
     struct sigaction sigact;
-
+    if(argc<2){
+        fprintf(stderr,"Give me filepath to script\n");
+        exit(EXIT_FAILURE);
+    }
     sigset_t sigset;
     sigemptyset(&sigset);
 
-
+    signal(SIGCHLD,child_died);
     sigact.sa_handler=sigtstp_handler;
     sigact.sa_mask=sigset;
     sigact.sa_flags=0;
@@ -55,17 +62,16 @@ int main(int argc, char** argv) {//sigint = signal, sigtstp= sigaction
 
     if((child_pid=fork())==0){
 
-        execvp(realpath("../zad1/script.sh",NULL),NULL);
+        execvp(realpath(argv[1],NULL),NULL);
+        perror("Following error occured executing the script");
         exit(EXIT_FAILURE);
+
     }
     if(child_pid)script_working=1;
 
     while(1){
 
         if(sigtstp_wait==0){ //when unstopped by sigtstp
-
-
-
 
         }
         sleep(1);
