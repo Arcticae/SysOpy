039ee4d0a0d63d2f8f19fe176509920d1fb67ff9
Arcticae <tomekgsd@gmail.com>
2018-04-26 19:37:57 +0200

    Improved code readability and some other stuff so it is simpler, than a stick of a pawian

--- lab6/zad1/server.c
+++ lab6/zad1/server.c
@@ -1,6 +1,7 @@
 //
 // Created by timelock on 25.04.18.
 //
+#define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
 #include <sys/msg.h>
@@ -13,108 +14,117 @@
 #include <fcntl.h>
 #include <time.h>
 #include <sys/time.h>
+#include <sys/stat.h>
 
-int end_received=0;
+int end_received = 0;
 
 key_t server_id;
 int server_qid;
 
 int client_qids[MAX_CLIENTS];
-int current_clients=0;
+int current_clients = 0;
 
-void handle_interrupt(int sig){
+void handle_interrupt(int sig) {
 
 
     exit(0);
 }
 
-void exit_procedure(){
+void exit_procedure() {
 
 
-    if(msgctl(server_qid,IPC_RMID,NULL)==-1){
+    if (msgctl(server_qid, IPC_RMID, NULL) == -1) {
         perror("Could not delete srv queue");
         exit(EXIT_FAILURE);
     }
-        current_clients--;
-    while(current_clients>0){
-        if(msgctl(client_qids[current_clients],IPC_RMID,NULL)==-1)
-        {
-            printf("Failed to delete client of id: %d\n",current_clients);
+    current_clients--;
+    while (current_clients > 0) {
+        if (msgctl(client_qids[current_clients], IPC_RMID, NULL) == -1) {
+            printf("Failed to delete client of id: %d\n", current_clients);
         }
         current_clients--;
     }
 }
 
-void start_server(){
+void start_server() {
 
     atexit(exit_procedure);                        //setting procedures for exit and sigs
-    signal(SIGINT,handle_interrupt);
-    server_id=ftok(getenv("HOME"),'s');            //s stands for serv
+    signal(SIGINT, handle_interrupt);
+    server_id = ftok(getenv("HOME"), 's');            //s stands for serv
 
-    if(server_id==-1){
+    if (server_id == -1) {
         perror("Error while getting the queuekey for srv");
         exit(EXIT_FAILURE);
     }
 
-    server_qid=msgget(server_id, IPC_CREAT | IPC_EXCL | S_IRWXU | S_IRWXG );
+    server_qid = msgget(server_id, IPC_CREAT | IPC_EXCL | S_IRWXU | S_IRWXG);
 
-    if(server_qid==-1){
-        server_qid=msgget(server_id,S_IRWXU | S_IRWXG );
-    }if(server_id==-1){
+    if (server_qid == -1) {
+        server_qid = msgget(server_id, S_IRWXU | S_IRWXG);
+    }
+    if (server_id == -1) {
         perror("Theres a problem still creating the quueue");
         exit(EXIT_FAILURE);
     }
 
 }
-void handle_INTRODUCE(struct msgbuffer_new*message,struct msgbuffer_new*response);
-void handle_CALC(struct msgbuffer_new*message,struct msgbuffer_new*response,int OPCODE);
-void handle_TIME(struct msgbuffer_new*message,struct msgbuffer_new*response);
-void handle_MIRROR(struct msgbuffer_new*message,struct msgbuffer_new*response);
-void remove_client(struct msgbuffer_new*message,struct msgbuffer_new*response);
-int main(int argc,char**argv){
+
+void handle_INTRODUCE(struct msgbuffer_new *message, struct msgbuffer_new *response);
+
+void handle_CALC(struct msgbuffer_new *message, struct msgbuffer_new *response, int OPCODE);
+
+void handle_TIME(struct msgbuffer_new *message, struct msgbuffer_new *response);
+
+void handle_MIRROR(struct msgbuffer_new *message, struct msgbuffer_new *response);
+
+void remove_client(struct msgbuffer_new *message, struct msgbuffer_new *response);
+
+int main(int argc, char **argv) {
 
     start_server(); //global qid,   's'  projname, $dir=home
 
-    while(1){
+    while (1) {
         struct msgbuffer_new message;
         struct msgbuffer_new response;
 
-        if(msgrcv(server_qid,&message,MESSAGE_SIZE,0,IPC_NOWAIT)==-1 ){      //we do not wait for the communicate, because we have infinite loop
-            if(errno!=ENOMSG) {
+        if (msgrcv(server_qid, &message, MESSAGE_SIZE, 0, IPC_NOWAIT) ==
+            -1) {      //we do not wait for the communicate, because we have infinite loop
+            if (errno != ENOMSG) {
                 perror("Error while fetching message from srv queue");
                 exit(EXIT_FAILURE);
-            }else if(end_received==1)exit(EXIT_SUCCESS);
+            } else if (end_received == 1)exit(EXIT_SUCCESS);
 
-        }else{
-            printf("I have received taskwith args: %s\tid:%d\topcode:%lo\n",message.message,message.index,message.mtype);
+        } else {
+            printf("I have received taskwith args: %s\tid:%d\topcode:%lo\n", message.message, message.index,
+                   message.mtype);
 
-            switch (message.mtype){
+            switch (message.mtype) {
                 case INTRODUCE:
-                    handle_INTRODUCE(&message,&response);
+                    handle_INTRODUCE(&message, &response);
                     break;
                 case ADD:
-                    handle_CALC(&message,&response,ADD);
+                    handle_CALC(&message, &response, ADD);
                     break;
                 case SUB:
-                    handle_CALC(&message,&response,SUB);
+                    handle_CALC(&message, &response, SUB);
                     break;
                 case MUL:
-                    handle_CALC(&message,&response,MUL);
+                    handle_CALC(&message, &response, MUL);
                     break;
                 case DIV:
-                    handle_CALC(&message,&response,DIV);
+                    handle_CALC(&message, &response, DIV);
                     break;
                 case TIME:
-                    handle_TIME(&message,&response);
+                    handle_TIME(&message, &response);
                     break;
                 case MIRROR:
-                    handle_MIRROR(&message,&response);
+                    handle_MIRROR(&message, &response);
                     break;
                 case END:
-                    end_received=1;
+                    end_received = 1;
                     break;
                 case GOODBYE:
-                    remove_client(&message,&response);
+                    remove_client(&message, &response);
                     continue;
                 default:
                     printf("Unnknown signal from sb\n");
@@ -123,8 +133,8 @@ int main(int argc,char**argv){
 
             }
             //respond after doing stuff
-            response.mtype=message.mtype;
-            if(msgsnd(client_qids[message.index],&response,MESSAGE_SIZE,0)==-1){
+            response.mtype = message.mtype;
+            if (msgsnd(client_qids[message.index], &response, MESSAGE_SIZE, 0) == -1) {
                 perror("Error while sending off response to client");
                 exit(EXIT_FAILURE);
             }
@@ -134,80 +144,79 @@ int main(int argc,char**argv){
 }
 
 
-void handle_INTRODUCE(struct msgbuffer_new*message,struct msgbuffer_new*response){
-    if(current_clients<MAX_CLIENTS){
+void handle_INTRODUCE(struct msgbuffer_new *message, struct msgbuffer_new *response) {
+    if (current_clients < MAX_CLIENTS) {
 
-        client_qids[current_clients]=msgget(message->queue_key, S_IRWXU | S_IRWXG);
+        client_qids[current_clients] = msgget(message->queue_key, S_IRWXU | S_IRWXG);
 
-        if(client_qids[current_clients]==-1){
+        if (client_qids[current_clients] == -1) {
             perror("Cannot get client's queue");
             exit(EXIT_FAILURE);
         }
-        response->mtype=INTRODUCE;
-        response->index=current_clients;
+        response->mtype = INTRODUCE;
+        response->index = current_clients;
         current_clients++;
 
-    }
-    else{
-        fprintf(stderr,"Max number of clients exceeded\nExiting...");
+    } else {
+        fprintf(stderr, "Max number of clients exceeded\nExiting...");
         exit(EXIT_FAILURE);
     }
 }
 
-void handle_CALC(struct msgbuffer_new*message,struct msgbuffer_new*response,int OPCODE){
-    int n1,n2;
-    char*c1,*c2;
-    if((c1=strtok(message->message," \t\n"))==NULL || (c2=strtok(NULL," \n\t"))==NULL){
-        sprintf(response->message,"Wrong format of arguments given by you.\n");
+void handle_CALC(struct msgbuffer_new *message, struct msgbuffer_new *response, int OPCODE) {
+    int n1, n2;
+    char *c1, *c2;
+    if ((c1 = strtok(message->message, " \t\n")) == NULL || (c2 = strtok(NULL, " \n\t")) == NULL) {
+        sprintf(response->message, "Wrong format of arguments given by you.\n");
 
-    }else{
+    } else {
         int res;
-        n1=(int)strtol(c1,NULL,10);
-        n2=(int)strtol(c2,NULL,10);
-        switch(OPCODE){
+        n1 = (int) strtol(c1, NULL, 10);
+        n2 = (int) strtol(c2, NULL, 10);
+        switch (OPCODE) {
             case ADD:
-                res=n1+n2;
+                res = n1 + n2;
                 break;
             case SUB:
-                res=n1-n2;
+                res = n1 - n2;
                 break;
             case MUL:
-                res=n1*n2;
+                res = n1 * n2;
                 break;
             case DIV:
-                res=n1/n2;
+                res = n1 / n2;
                 break;
             default:
-                fprintf(stderr,"No such operation, error.");
-                res=0;
+                fprintf(stderr, "No such operation, error.");
+                res = 0;
                 break;
         }
-        sprintf(response->message,"%d",res);
+        sprintf(response->message, "%d", res);
     }
 }
 
-void handle_TIME(struct msgbuffer_new*message,struct msgbuffer_new*response){
+void handle_TIME(struct msgbuffer_new *message, struct msgbuffer_new *response) {
     struct timeval timeBuffer;
     gettimeofday(&timeBuffer, NULL);
-    struct tm * timestruct = localtime(&timeBuffer.tv_sec);
+    struct tm *timestruct = localtime(&timeBuffer.tv_sec);
     strftime(response->message, MESSAGE_SIZE, "%Y-%m-%d %H:%M:%S", timestruct);
 }
 
-void handle_MIRROR(struct msgbuffer_new*message,struct msgbuffer_new*response){
-    size_t len=strlen(message->message);
+void handle_MIRROR(struct msgbuffer_new *message, struct msgbuffer_new *response) {
+    size_t len = strlen(message->message);
     int i;
-    for(i=0;i<len;i++){
-        response->message[len-1-i]=message->message[i];
+    for (i = 0; i < len; i++) {
+        response->message[len - 1 - i] = message->message[i];
     }
-    response->message[len]='\0';
+    response->message[len] = '\0';
 
 }
 
-void remove_client(struct msgbuffer_new*message,struct msgbuffer_new*response){
+void remove_client(struct msgbuffer_new *message, struct msgbuffer_new *response) {
 
     int i;
-    for (i=message->index;i<current_clients;i++){
-        client_qids[i]=client_qids[i+1];
+    for (i = message->index; i < current_clients; i++) {
+        client_qids[i] = client_qids[i + 1];
     }
     current_clients--;
 
--- lab6/zad2/server.c
+++ lab6/zad2/server.c
@@ -3,6 +3,7 @@
 //
 #define _POSIX_C_SOURCE 200809L
 #define _XOPEN_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -15,96 +16,113 @@
 #include <mqueue.h>
 #include <sys/stat.h>
 
-int end_received=0;
+int end_received = 0;
 
 mqd_t server_queue;
 mqd_t client_queues[MAX_CLIENTS];
 ssize_t ercode;
-int current_clients=0;
+int current_clients = 0;
 
-void handle_interrupt(int sig){
-
-
+void handle_interrupt(int sig) {
     exit(0);
 }
 
-void exit_procedure(){
+void exit_procedure() {
 
     mq_close(server_queue);
     mq_unlink(SRV_DIRECTORY);
 
     current_clients--;
-    while(current_clients>0){
+    while (current_clients > 0) {
         mq_close(client_queues[current_clients]);
         current_clients--;
     }
 }
 
-void start_server(){
+void start_server() {
 
     atexit(exit_procedure);                        //setting procedures for exit and sigs
-    signal(SIGINT,handle_interrupt);
+    signal(SIGINT, handle_interrupt);
 
     struct mq_attr server_attributes;
-    server_attributes.mq_msgsize = sizeof(struct msgbuffer_new);
+    server_attributes.mq_msgsize = MESSAGE_SIZE;
     server_attributes.mq_maxmsg = 10;
 
-    server_queue= mq_open(SRV_DIRECTORY, O_RDONLY | O_CREAT | O_EXCL | O_NONBLOCK, S_IRWXU | S_IRWXG ,&server_attributes);
-    if(server_queue==-1){
+    server_queue = mq_open(SRV_DIRECTORY, O_RDONLY | O_CREAT | O_EXCL | O_NONBLOCK, S_IRWXU | S_IRWXG,
+                           &server_attributes);
+    if (server_queue == -1) {
         perror("Error creating queue 4 serv");
         exit(EXIT_FAILURE);
     }
 }
-void handle_INTRODUCE(struct msgbuffer_new*message,struct msgbuffer_new*response);
-void handle_CALC(struct msgbuffer_new*message,struct msgbuffer_new*response,int OPCODE);
-void handle_TIME(struct msgbuffer_new*message,struct msgbuffer_new*response);
-void handle_MIRROR(struct msgbuffer_new*message,struct msgbuffer_new*response);
-void remove_client(struct msgbuffer_new*message,struct msgbuffer_new*response);
 
-int main(int argc,char**argv){
+void send_message(mqd_t queue_id, struct msgbuffer_new *message) {
+    do {
+        ercode = mq_send(queue_id, (char *) message, MESSAGE_SIZE, 0);
+        if (ercode == -1 && errno != EAGAIN) {
+            perror("Error while sending off response to client");
+            exit(EXIT_FAILURE);
+        }
+    } while (ercode == -1);
+}
+
+
+
+void handle_INTRODUCE(struct msgbuffer_new *message, struct msgbuffer_new *response);
+
+void handle_CALC(struct msgbuffer_new *message, struct msgbuffer_new *response, int OPCODE);
+
+void handle_TIME(struct msgbuffer_new *message, struct msgbuffer_new *response);
+
+void handle_MIRROR(struct msgbuffer_new *message, struct msgbuffer_new *response);
+
+void remove_client(struct msgbuffer_new *message, struct msgbuffer_new *response);
+
+int main(int argc, char **argv) {
 
     start_server();
 
-    while(1){
+    while (1) {
         struct msgbuffer_new message;
         struct msgbuffer_new response;
 
-        if(mq_receive(server_queue,(char*) &message,sizeof(struct msgbuffer_new),0)==-1 ){      //we do not wait for the communicate, because we have infinite loop
-            if(errno!=EAGAIN) {
+        if (mq_receive(server_queue, (char *) &message, MESSAGE_SIZE, 0) ==-1) {      //we do not wait for the communicate, because we have infinite loop
+            if (errno != EAGAIN) {
                 perror("Error while fetching message from srv queue");
                 exit(EXIT_FAILURE);
-            }else if(end_received==1)exit(EXIT_SUCCESS);
+            } else if (end_received == 1)exit(EXIT_SUCCESS);
 
-        }else{
-            printf("I have received taskwith args: %s\tid:%d\topcode:%lo\n",message.message,message.index,message.mtype);
+        } else {
+            printf("I have received taskwith args: %s\tid:%d\topcode:%lo\n", message.message, message.index,
+                   message.mtype);
 
-            switch (message.mtype){
+            switch (message.mtype) {
                 case INTRODUCE:
-                    handle_INTRODUCE(&message,&response);
+                    handle_INTRODUCE(&message, &response);
                     break;
                 case ADD:
-                    handle_CALC(&message,&response,ADD);
+                    handle_CALC(&message, &response, ADD);
                     break;
                 case SUB:
-                    handle_CALC(&message,&response,SUB);
+                    handle_CALC(&message, &response, SUB);
                     break;
                 case MUL:
-                    handle_CALC(&message,&response,MUL);
+                    handle_CALC(&message, &response, MUL);
                     break;
                 case DIV:
-                    handle_CALC(&message,&response,DIV);
+                    handle_CALC(&message, &response, DIV);
                     break;
                 case TIME:
-                    handle_TIME(&message,&response);
+                    handle_TIME(&message, &response);
                     break;
                 case MIRROR:
-                    handle_MIRROR(&message,&response);
+                    handle_MIRROR(&message, &response);
                     break;
                 case END:
-                    end_received=1;
+                    end_received = 1;
                     break;
                 case GOODBYE:
-                    remove_client(&message,&response);
+                    remove_client(&message, &response);
                     continue;
                 default:
                     printf("Unnknown signal from sb\n");
@@ -113,97 +131,88 @@ int main(int argc,char**argv){
 
             }
             //respond after doing stuff
-            response.mtype=message.mtype;
-            do{
-                ercode=mq_send(client_queues[message.index],(char*)&response,sizeof(struct msgbuffer_new),0);
-                if(ercode==-1 && errno!=EAGAIN){
-                perror("Error while sending off response to client");
-                exit(EXIT_FAILURE);
-                }
-            }while(ercode==-1);
+            response.mtype = message.mtype;
+            send_message(client_queues[message.index],&response);
         }
 
     }
 }
 
 
-void handle_INTRODUCE(struct msgbuffer_new*message,struct msgbuffer_new*response){
-    if(current_clients<MAX_CLIENTS){
+void handle_INTRODUCE(struct msgbuffer_new *message, struct msgbuffer_new *response) {
+    if (current_clients < MAX_CLIENTS) {
 
-        client_queues[current_clients]=mq_open(message->message,O_WRONLY |  O_NONBLOCK);
+        client_queues[current_clients] = mq_open(message->message, O_WRONLY | O_NONBLOCK);
 
-        if(client_queues[current_clients]==-1){
+        if (client_queues[current_clients] == -1) {
             perror("Cannot get client's queue");
             exit(EXIT_FAILURE);
         }
 
-        response->mtype=INTRODUCE;
-        response->index=current_clients;
+        response->mtype = INTRODUCE;
+        response->index = current_clients;
         current_clients++;
 
-    }
-    else{
-        fprintf(stderr,"Max number of clients exceeded\nExiting...");
+    } else {
+        fprintf(stderr, "Max number of clients exceeded\nExiting...");
         exit(EXIT_FAILURE);
     }
 }
 
-void handle_CALC(struct msgbuffer_new*message,struct msgbuffer_new*response,int OPCODE){
-    int n1,n2;
-    char*c1,*c2;
-    if((c1=strtok(message->message," \t\n"))==NULL || (c2=strtok(NULL," \n\t"))==NULL){
-        sprintf(response->message,"Wrong format of arguments given by you.\n");
+void handle_CALC(struct msgbuffer_new *message, struct msgbuffer_new *response, int OPCODE) {
+    int n1, n2;
+    char *c1, *c2;
+    if ((c1 = strtok(message->message, " \t\n")) == NULL || (c2 = strtok(NULL, " \n\t")) == NULL) {
+        sprintf(response->message, "Wrong format of arguments given by you.\n");
 
-    }else{
+    } else {
         int res;
-        n1=(int)strtol(c1,NULL,10);
-        n2=(int)strtol(c2,NULL,10);
-        switch(OPCODE){
+        n1 = (int) strtol(c1, NULL, 10);
+        n2 = (int) strtol(c2, NULL, 10);
+        switch (OPCODE) {
             case ADD:
-                res=n1+n2;
+                res = n1 + n2;
                 break;
             case SUB:
-                res=n1-n2;
+                res = n1 - n2;
                 break;
             case MUL:
-                res=n1*n2;
+                res = n1 * n2;
                 break;
             case DIV:
-                res=n1/n2;
+                res = n1 / n2;
                 break;
             default:
-                fprintf(stderr,"No such operation, error.");
-                res=0;
+                fprintf(stderr, "No such operation, error.");
+                res = 0;
                 break;
         }
-        sprintf(response->message,"%d",res);
+        sprintf(response->message, "%d", res);
     }
 }
 
-void handle_TIME(struct msgbuffer_new*message,struct msgbuffer_new*response){
+void handle_TIME(struct msgbuffer_new *message, struct msgbuffer_new *response) {
     struct timeval timeBuffer;
     gettimeofday(&timeBuffer, NULL);
-    struct tm * timestruct = localtime(&timeBuffer.tv_sec);
+    struct tm *timestruct = localtime(&timeBuffer.tv_sec);
     strftime(response->message, MESSAGE_SIZE, "%Y-%m-%d %H:%M:%S", timestruct);
 }
 
-void handle_MIRROR(struct msgbuffer_new*message,struct msgbuffer_new*response){
-    size_t len=strlen(message->message);
+void handle_MIRROR(struct msgbuffer_new *message, struct msgbuffer_new *response) {
+    size_t len = strlen(message->message);
     int i;
-    for(i=0;i<len;i++){
-        response->message[len-1-i]=message->message[i];
+    for (i = 0; i < len; i++) {
+        response->message[len - 1 - i] = message->message[i];
     }
-    response->message[len]='\0';
+    response->message[len] = '\0';
 
 }
 
-void remove_client(struct msgbuffer_new*message,struct msgbuffer_new*response){
+void remove_client(struct msgbuffer_new *message, struct msgbuffer_new *response) {
 
     int i;
-    for (i=message->index;i<current_clients;i++){
-        client_queues[i]=client_queues[i+1];
+    for (i = message->index; i < current_clients; i++) {
+        client_queues[i] = client_queues[i + 1];
     }
     current_clients--;
-
-
 }
